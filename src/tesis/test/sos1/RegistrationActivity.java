081025480e3ab940435c82b227e3bf9459076479
Francisco Alonso <francisco.a.alonso.s@gmail.com>
2014-01-09 21:11:22 -0430

    registration

--- src/tesis/test/sos1/RegistrationActivity.java
+++ src/tesis/test/sos1/RegistrationActivity.java
@@ -2,14 +2,44 @@ package tesis.test.sos1;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
+
+import org.doubango.ngn.NgnEngine;
+import org.doubango.ngn.events.NgnEventArgs;
+import org.doubango.ngn.events.NgnRegistrationEventArgs;
+import org.doubango.ngn.services.INgnConfigurationService;
+import org.doubango.ngn.services.INgnSipService;
+import org.doubango.ngn.utils.NgnConfigurationEntry;
+import org.doubango.ngn.utils.NgnStringUtils;
 
 public class RegistrationActivity extends Activity {
 
+	private BroadcastReceiver mSipBroadCastRecv;
+	private final NgnEngine mEngine;
+	private final INgnConfigurationService mConfigurationService;
+	private final INgnSipService mSipService;
+	
+	public RegistrationActivity(){
+		mEngine = NgnEngine.getInstance();
+		mConfigurationService = mEngine.getConfigurationService();
+		mSipService = mEngine.getSipService();
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_registration);
+		
+		// Subscribe for registration state changes
+        mSipBroadCastRecv = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				final String action = intent.getAction();
+			}
+        };
 	}
 
 	@Override
